46b3e24024c735ad68adeb5ce4025a4fdf6f3de6
ChengTX666 <873661351@qq.com>
2025-01-24 13:34:28 +0800

    update

--- mynetty/src/main/java/nefu/netty/App.java
+++ mynetty/src/main/java/nefu/netty/App.java
@@ -1,14 +1,9 @@
-package nefu;
+package nefu.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.sctp.nio.NioSctpServerChannel;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-
-import java.nio.channels.SelectionKey;
 
 /**
  * Hello world!
