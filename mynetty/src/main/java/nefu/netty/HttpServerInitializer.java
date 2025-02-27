505f8ad8d9c959a89f5fc6ebb00c8a7e3147983c
ChengTX666 <873661351@qq.com>
2025-02-27 17:40:09 +0800

    update

--- mynetty/src/main/java/nefu/netty/HttpServerInitializer.java
+++ mynetty/src/main/java/nefu/netty/HttpServerInitializer.java
@@ -1,4 +1,4 @@
-package nefu;
+package nefu.netty;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
---
+++ mynetty/src/main/java/nefu/netty/MyChannelHandler.java
@@ -0,0 +1,21 @@
+package nefu.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+
+public class MyChannelHandler extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        //等于
+//        pipeline.addLast("httpResponseEndcoder", new HttpResponseEncoder());
+//        pipeline.addLast("HttpRequestDecoder", new HttpRequestDecoder());
+
+    }
+}
